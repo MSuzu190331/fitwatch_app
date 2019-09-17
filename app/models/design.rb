9462977132bdf84a5546afea9f660fc3ab16f278
MSuzu190331 <m.suzuki.0u0@gmail.com>
2019-09-17 18:36:14 +0900

    add active_hash

---
+++ app/models/design.rb
@@ -0,0 +1,10 @@
+class Design < ActiveHash::Base
+  self.data = [
+    {:id => 1, :name => "クラシック"},
+    {:id => 2, :name => "フェミニン"},
+    {:id => 3, :name => "カジュアル"},
+    {:id => 4, :name => "シンプル"},
+    {:id => 5, :name => "メンズライク"},
+    {:id => 6, :name => "スポーティ"},
+  ]
+end
