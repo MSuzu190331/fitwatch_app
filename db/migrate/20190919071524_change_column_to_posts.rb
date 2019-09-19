7470c5452da42c1adbe4f27f559804bd9b63c5dd
MSuzu190331 <m.suzuki.0u0@gmail.com>
2019-09-19 17:59:03 +0900

    update design_select_form

--- db/migrate/20190919071524_change_column_to_posts.rb
+++ db/migrate/20190919071524_change_column_to_posts.rb
@@ -1,6 +1,7 @@
 class ChangeColumnToPosts < ActiveRecord::Migration[5.2]
   def change
     rename_column :posts, :design, :name_id
-    change_column :posts, :name_id, :integer
+    change_column :posts, :name_id, 'integer USING CAST(column_name AS integer)'
+    # change_column :posts, :name_id, :integer
   end
 end
