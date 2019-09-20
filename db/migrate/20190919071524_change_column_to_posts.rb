616c420dbb7d4a32dd574c360f9de07f536e5b1e
MSuzu190331 <m.suzuki.0u0@gmail.com>
2019-09-20 15:09:12 +0900

    update migrate

--- db/migrate/20190919071524_change_column_to_posts.rb
+++ db/migrate/20190919071524_change_column_to_posts.rb
@@ -1,7 +1,7 @@
 class ChangeColumnToPosts < ActiveRecord::Migration[5.2]
   def change
     rename_column :posts, :design, :name_id
-    # change_column :posts, :name_id, 'integer USING CAST(name_id AS integer)'
+    change_column :posts, :name_id, 'integer USING CAST(name_id AS integer)'
     # change_column :posts, :name_id, :integer
   end
 end
