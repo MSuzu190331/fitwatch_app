588a37cd67fa623656bedb69e3f9b4e0ad8b2323
MSuzu190331 <m.suzuki.0u0@gmail.com>
2019-09-25 19:58:39 +0900

    update comment and views

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
       get 'ranking'
     end
     resources :likes, only: [:create, :destroy]
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
 
 end
