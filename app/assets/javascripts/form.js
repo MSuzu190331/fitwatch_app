a85c925eee1c2b5966be92352c9f03296b95aa2f
MSuzu190331 <m.suzuki.0u0@gmail.com>
2019-09-26 15:28:24 +0900

    delete console.log

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -2,19 +2,15 @@ $(function() {
   function readURL(input) {
     if (input.files && input.files[0]) {
       var reader = new FileReader();
-      console.log('ok2');
       reader.onload = function (e) {
         $("#image_prev").attr("src", e.target.result);
-        console.log('ok3');
       }
       reader.readAsDataURL(input.files[0]);
-      console.log('ok4');
     }
   }
   $(document).on('turbolinks:load',function(){
     $("#form_image").change(function(){
       readURL(this);
-      console.log('ok1');
     });
   });
 });
