bf4c1e772a6053c90d8f79f7fabdb3b57fba3a31
MSuzu190331 <m.suzuki.0u0@gmail.com>
2019-09-27 19:54:01 +0900

    create category-search

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,22 @@
 class Post < ApplicationRecord
 # class Post < ActiveRecord::Base
   validates :name_id, :about, :image, presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :design
+
   mount_uploader :image, ImageUploader
+
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  def self.search(search)
+    # return Post.all unless search
+    # Post.find_by(name_id: search)
+    return Post.all unless search
+    Post.where(['name_id LIKE ?', "%#{search}%"])
+  end
+
 end
